3bf34314475266c4b337a74833bbc4b43784979b
Aditya Hajare <aditya43@gmail.com>
2021-05-09 09:26:29 +0530

    Check if request is public or not

--- oauth/oauth.go
+++ oauth/oauth.go
@@ -1,5 +1,18 @@
 package oauth
 
+import "net/http"
+
+const (
+	headerXPublic = "X-Public"
+)
+
 type oauthClient struct{}
 
 type oauthInterface interface{}
+
+func IsPublic(request *http.Request) bool {
+	if request == nil {
+		return true
+	}
+	return request.Header.Get(headerXPublic) == "true"
+}
