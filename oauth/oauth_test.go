5af21bead0eca7c1d703b853de35997603998839
Aditya Hajare <aditya43@gmail.com>
2021-05-12 19:29:12 +0530

    TestIsPublicNoError

--- oauth/oauth_test.go
+++ oauth/oauth_test.go
@@ -1,6 +1,7 @@
 package oauth
 
 import (
+	"net/http"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -15,4 +16,15 @@ func TestOauthConstants(t *testing.T) {
 
 func TestIsPublicNilRequest(t *testing.T) {
 	assert.True(t, IsPublic(nil))
+}
+
+func TestIsPublicNoError(t *testing.T) {
+	request := http.Request{
+		Header: make(http.Header),
+	}
+
+	assert.False(t, IsPublic(&request))
+
+	request.Header.Add("X-Public", "true")
+	assert.True(t, IsPublic(&request))
 }
